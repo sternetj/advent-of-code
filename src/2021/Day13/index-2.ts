08cc630087e3382b8d33b2d233283743eed47387
Teddy Sterne <teddy.sterne@lifeomic.com>
2021-12-13 09:47:48 -0500

    2021 Day 13

---
+++ src/2021/Day13/index-2.ts
@@ -0,0 +1,56 @@
+import { parseInput } from "../../util/parse-input";
+import { uniqBy } from "lodash";
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+const input = parseInput(
+  (l) => {
+    if (l.startsWith("fold along")) {
+      const [_, dir, num] = l.match(/.*([xy])=(\d+)/);
+      return { [dir]: Number(num) } as Partial<Point>;
+    } else {
+      const [x, y] = l.split(",").map(Number);
+      return { x, y };
+    }
+  },
+  __dirname,
+  "input.txt"
+);
+
+let points = input.filter((p) => "x" in p && "y" in p) as Point[];
+const folds = input.filter((p) => !("x" in p && "y" in p));
+
+const printPoints = (p: Point[]) => {
+  const maxX = Math.max(...p.map((p) => p.x));
+  const maxY = Math.max(...p.map((p) => p.y));
+  let output = "";
+
+  for (let y = 0; y <= maxY; y++) {
+    for (let x = 0; x <= maxX; x++) {
+      const hasPoint = p.some((p) => p.x == x && p.y == y);
+
+      output += hasPoint ? "#" : ".";
+    }
+    output += "\n";
+  }
+
+  console.log(output);
+};
+
+folds.forEach((fold) => {
+  const dir = "x" in fold ? "x" : "y";
+  const foldDim = fold[dir];
+
+  points = uniqBy(
+    points.map((p) => ({
+      ...p,
+      [dir]: p[dir] >= foldDim ? foldDim - (p[dir] - foldDim) : p[dir],
+    })),
+    (p) => `${p.x},${p.y}`
+  );
+});
+
+printPoints(points);
