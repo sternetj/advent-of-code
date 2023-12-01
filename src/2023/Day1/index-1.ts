b6e9c87b4a67dafacaf364b9b0fd4778bcbed21a
Teddy Sterne <sternetj@gmail.com>
2023-12-01 07:43:17 -0500

    Upgrade Packages

--- src/2023/Day1/index-1.ts
+++ src/2023/Day1/index-1.ts
@@ -1,9 +1,16 @@
 import { parseInput } from "../../util/parse-input";
 
-const input = parseInput((l) => {
-  const first = l.split("").find(v => isFinite(parseInt(v)));
-  const last = l.split("").reverse().find(v => isFinite(parseInt(v)));
-  return Number(first+last);
-},__dirname, process.env.INPUT);
+const input = parseInput(
+  (l) => {
+    const first = l.split("").find((v) => isFinite(parseInt(v)));
+    const last = l
+      .split("")
+      .reverse()
+      .find((v) => isFinite(parseInt(v)));
+    return Number(first + last);
+  },
+  __dirname,
+  process.env.INPUT
+);
 
 console.log(input.reduce((t, v) => t + v, 0));
--- src/2023/Day1/index-2.ts
+++ src/2023/Day1/index-2.ts
@@ -1,26 +1,34 @@
 import { parseInput } from "../../util/parse-input";
 
-const re = /([0-9]|one|two|three|four|five|six|seven|eight|nine|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)/i;
+const re =
+  /([0-9]|one|two|three|four|five|six|seven|eight|nine|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)/i;
 
 const values = {
-  "zero": "0",
-  "one": "1",
-  "two": "2",
-  "three": "3",
-  "four": "4",
-  "five": "5",
-  "six": "6",
-  "seven": "7",
-  "eight": "8",
-  "nine": "9",
-}
+  zero: "0",
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
 
-const sub = (v: string) => values[v.toLowerCase()] ?? values[v.split("").reverse().join("").toLowerCase()] ?? v;
+const sub = (v: string) =>
+  values[v.toLowerCase()] ??
+  values[v.split("").reverse().join("").toLowerCase()] ??
+  v;
 
-const input = parseInput((l) => {
-  const [first] = l.match(re);
-  const [last] = l.split("").reverse().join("").match(re);
-  return Number(sub(first)+sub(last));
-},__dirname, process.env.INPUT);
+const input = parseInput(
+  (l) => {
+    const [first] = l.match(re);
+    const [last] = l.split("").reverse().join("").match(re);
+    return Number(sub(first) + sub(last));
+  },
+  __dirname,
+  process.env.INPUT
+);
 
 console.log(input.reduce((t, v) => t + v, 0));
