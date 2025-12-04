f6a15c732595adf0eb3b7e788dd351c9a39886b6
Teddy Sterne <teddy.sterne@fountainlife.com>
2025-12-04 09:48:46 -0500

    2025 Day 4

---
+++ src/2025/Day4/index-1.ts
@@ -0,0 +1,21 @@
+import { parseInput } from "../../util/parse-input";
+
+const input = parseInput((l) => l.split(""));
+
+const result = input.reduce((total, line, row) => {
+  for (let col = 0; col < line.length; col++) {
+    if (line[col] !== "@") continue;
+    let count = 0;
+    for (let cx = -1; cx <= 1; cx++) {
+      for (let rx = -1; rx <= 1; rx++) {
+        const check = (input[row + rx] || [])[col + cx];
+        if (check === "@") count++;
+      }
+    }
+
+    if (count <= 4) total++;
+  }
+  return total;
+}, 0);
+
+console.log(result);
